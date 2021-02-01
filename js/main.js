6286c7b33942c0bb29fe2da374efc2fa77efcfa4
rjvelazco <rjvelazco21@gmail.com>
2021-01-31 21:12:01 -0400

    Widget Terminado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 
+    const search = document.querySelector('.search'),
+    btn = document.querySelector('.btn'),
+    input = document.querySelector('.search-input');
 
+    btn.addEventListener('click', ()=>{
+        search.classList.toggle('show-input');
+    });
 
-
+    input.addEventListener('focusout',()=>{
+        search.classList.remove('show-input');
+    });
 
